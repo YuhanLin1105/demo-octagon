a258c597f79140973c2c2f03432ce277affbea6a
Yuhan Lin <yuhanlin1105@gmail.com>
2018-01-15 23:51:49 -0500

    init project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Layout from './containers/Layout/Layout';
+
+
+class App extends Component {
+  render() {
+    return (
+       {/* A JSX comment, this will work :)
+          <div>
+              sdf
+          </div>
+        */}
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Layout.css';
+
+class Layout extends Component {
+
+
+    render() {
+        return (
+            <div>
+                Layout
+            </div>
+        );
+
+    };
+};
+
+export default Layout;
